d9165c9eb84770209dc057ff5558e4816dca0d30
cezkid <cesar@enrriquez.com>
2021-10-27 12:40:49 -0400

    configure tailwind colors. disable .container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'disqo-blue': '#39ACDC',
+        'disqo-red' : '#EC3646',
+        'disqo-green' : '#57B93E'
+      }
+    },
   },
   variants: {
     extend: {},
+  },
+  corePlugins: {
+    //disable default container class
+    container: false
   },
   plugins: [],
 }
