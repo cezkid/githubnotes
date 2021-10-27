f3d12fa3f8541833d48f12f287abf5549f6af122
cezkid <cesar@enrriquez.com>
2021-10-27 18:32:47 -0400

    incorporate the githubRest helper

--- src/components/Notepad.js
+++ src/components/Notepad.js
@@ -1,13 +1,48 @@
 import React, { useState, useEffect } from "react";
+import GithubRest from "./GithubRest";
 
 const Notepad = ()=> {
-  const [title, setTitle] = useState('');
-  const [notes, setNotes] = useState({});
 
-  const createNotepad = ()=>{}
-  const readNotepad = ()=> {}
-  const updateNotepad = ()=> {}
-  const deleteNotepad = ()=> {}
+
+  const testData = {
+    description: "Nopepad Title",
+    files: {
+      "Note 1": {
+        content:"Note 1 Content"
+      },
+      "Note 2": {
+        content:"Note 2 Content"
+      }
+    }
+  }
+
+  const [action, setAction] = useState('');
+  const [data, setData] = useState({});
+  const [gistID, setGistID] = useState('');
+
+console.log(gistID);
+
+  const git = GithubRest(action, data, gistID, setGistID);
+
+  const createNotepad = ()=>{
+    setAction("POST");
+    setData(testData);
+  }
+  const readNotepad = ()=> {
+  
+
+  }
+  const updateNotepad = ()=> {
+    setAction("PATCH");
+    setData(testData);
+
+  }
+  const deleteNotepad = ()=> {
+    setAction("DELETE")
+    setData();
+  }
+
+  return { createNotepad, readNotepad, updateNotepad, deleteNotepad }
 }
 
 export default Notepad
