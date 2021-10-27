26d5dbf1d2ba7e6ca47a9184d2302b2af63d22f2
cezkid <cesar@enrriquez.com>
2021-10-27 12:42:20 -0400

    import classnames and add conditional colors

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
+var classNames = require('classnames');
 
-const Button = ({ children, onClick, ...props })=> {
+const Button = ({ children, variant, onClick, ...props })=> {
 
   return(
-    <button>
+    <button className={
+      classNames(
+      "text-white rounded px-5 py-3 leading-none",
+      {
+        "bg-gray-500" : !variant,
+        "bg-disqo-blue" : variant === "save",
+        "bg-disqo-red" : variant === "delete",
+        "bg-disqo-green" : variant === "add"
+      }
+    )}>
       {children || '' }
     </button>
   )
