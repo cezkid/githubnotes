b7e9240a2fec80dd28c2a7dcf1c5092159eeeea4
cezkid <cesar@enrriquez.com>
2021-10-27 14:07:42 -0400

    create Notepad object and functions

---
+++ src/components/Notepad.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const Notepad = ()=> {
+  const [title, setTitle] = useState('');
+  const [notes, setNotes] = useState({});
+
+  const createNotepad = ()=>{}
+  const readNotepad = ()=> {}
+  const updateNotepad = ()=> {}
+  const deleteNotepad = ()=> {}
+}
+
+export default Notepad
