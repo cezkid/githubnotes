7430dc86d6a5beb66eb5e7455475a7cbe987c663
cezkid <cesar@enrriquez.com>
2021-10-27 18:32:07 -0400

    allow to pass states to our helper class

--- src/components/GithubRest.js
+++ src/components/GithubRest.js
@@ -3,65 +3,47 @@ import { useState, useEffect } from "react";
 
 import githubToken from "../githubtoken";
 
-/**
- * 
- * Helper function for github API
- * @param {string} method - Defines method and is used to switch REST location.
- * @param {Object} data - data being sent on request.
- */
-const GithubRest = (method, data)=> {
+const GithubRest = (method, data, gistID, setGistID)=> {
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [gistID, setgistID] = useState(null);
 
- const returnURL = ()=> {
-    switch (method) {
-      //create gist
-      case 'POST':
-        return 'gists';
-        break;
-      //update gist
-      case 'PATCH':
-        return `gists/${gistID}`;
-        break;
-      //delete gist
-      case 'DELETE':
-        return `gists/${gistID}`;
-        break;
-      default: return null;
-    }
-  }
-  console.log(returnURL())
-
-  const testData = {
-    description: "Nopepad Title",
-    files: {
-      "Note 1": {
-        content:"Note 1 Content"
-      },
-      "Note 2": {
-        content:"Note 2 Content"
+  const returnURL = ()=> {
+      switch (method) {
+        //create gist
+        case 'POST':
+          return 'gists';
+          break;
+        //update gist
+        case 'PATCH':
+          return `gists/${gistID}`;
+          break;
+        //delete gist
+        case 'DELETE':
+          return `gists/${gistID}`;
+          break;
+        default: return null;
       }
     }
-  }
-
+  console.log(returnURL())
 
   useEffect(()=> {
-    const requestOptions ={
-      method: 'POST',
+    const requestOptions = {
+      method: method,
       headers: {
         Accept: 'application/vnd.github.v3+json',
         Authorization: `token ${githubToken}`
       },
-      body: JSON.stringify(testData)
+      body: JSON.stringify(data)
     }
+
+
     fetch(`https://api.github.com/${returnURL()}`,requestOptions)
       .then(response => response.json() )
       .then(
         (result) => {
           setIsLoaded(true);
-          setgistID(result);
+          setGistID(result.id);
           console.log(result)
       },
         (error) => {
@@ -70,8 +52,13 @@ const GithubRest = (method, data)=> {
           console.log(error)
         }
       )
-  }, []);
+  }, [method]);
 
+  return (
+    gistID
+  )
 }
+
+
 
 export default GithubRest
