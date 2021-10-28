e362033cc31b15f952153c51dc6feeffad9c5838
cezkid <cesar@enrriquez.com>
2021-10-28 08:36:19 -0400

    check if fields empty logic

--- src/components/NoteCreate.js
+++ src/components/NoteCreate.js
@@ -7,15 +7,28 @@ const NoteCreate = ({notes,setNotes})=> {
   const [title,setTitle] = useState('')
   const [content,setContent] = useState('')
 
+  //use reduce to get largest id number
+  const lastId =  notes.reduce((max, note) => (Number(note.id) > max ? Number(note.id): max), 0);
+
   return(
     <form className="flex flex-col gap-1">
-      <Note setTitle={setTitle} setContent={setContent}/>
+      <Note title={title} content={content} setTitle={setTitle} setContent={setContent}/>
       <div>
       <Button
         variant="add"
         onClick={ (e)=>{
           e.preventDefault(); 
-          setNotes([...notes,{title:title,content:content}])
+          if(!title){
+            console.log('please fill note title')
+          }
+          else if (!content){
+            console.log('please fill note')
+          }
+          else{
+          setNotes([...notes,{title:title,content:content,id:lastId + 1}]);
+          setTitle('');
+          setContent('');
+          }
           }
         }
         >Add</Button>
