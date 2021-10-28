9422bd5bef6b0efb0803cc0a5a9885480892c52d
cezkid <cesar@enrriquez.com>
2021-10-28 01:19:51 -0400

    clean up unused code

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,28 +3,11 @@ import Input from "./elements/Input";
 import TextArea from "./elements/TextArea";
 
 const Note = ({title, content, setTitle, setContent})=> {
-
-  // const [titleField, setTitleField] = useState('');
-  // const [contentField, setContentField] = useState('');
-
-  const createNote = (title,content)=> {
-  }
-
-  const readNote = ()=> {
-
-  }
-
-  const updateNote = ()=> {
-
-  }
-
-  const deleteNote = ()=> {
-
-  }
-  return (
+      return (
     <>
       <Input
         placeholder="Enter note title"
+
         value={title}
         onChange={(e)=>{setTitle(e.target.value)}}
       />
