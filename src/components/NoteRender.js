86c1377348b284c79a48d2098e93f24f2b9db518
cezkid <cesar@enrriquez.com>
2021-10-28 08:33:30 -0400

    allow to update the notes array on field change

--- src/components/NoteRender.js
+++ src/components/NoteRender.js
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./elements/Button";
 
 import Note from "./Note";
 
-const NoteRender = ({ note, notes })=> {
+const NoteRender = ({ note, notes, setNotes })=> {
 
   const [title,setTitle] = useState(note.title)
   const [content,setContent] = useState(note.content)
 
+  //get the index of note and adjust title and content
+  useEffect(() => {
+
+    let noteIndex = notes.findIndex(n => n.id === note.id);
+    notes[noteIndex].title = title;
+    notes[noteIndex].content = content;
+    
+  }, [title,content])
   return(
     <div className="flex flex-wrap gap-2">
       <div className="flex flex-col gap-1">
@@ -16,7 +24,11 @@ const NoteRender = ({ note, notes })=> {
       <div>
         <Button
           variant="delete"
-          onClick={ ()=>{console.log('delete') }}
+          onClick={ ()=>{
+            setNotes(
+              notes.filter(n => n.id !== note.id)
+            )
+            }}
           >
           Delete
           </Button>
