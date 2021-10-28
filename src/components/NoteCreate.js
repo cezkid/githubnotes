c7e320498ef6cebb461229ab447391e3b055d1c2
cezkid <cesar@enrriquez.com>
2021-10-27 23:20:16 -0400

    add logic to add to array

--- src/components/NoteCreate.js
+++ src/components/NoteCreate.js
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./elements/Button";
 import Input from "./elements/Input";
 import TextArea from "./elements/TextArea";
 
-const NoteCreate = ()=> {
+const NoteCreate = ({notes,notesFunc})=> {
+
+  const [title,setTitle] = useState('')
+  const [content,setContent] = useState('')
 
   return(
     <form className="flex flex-col gap-1">
-      <Input placeholder="Enter note title"/>
-      <TextArea placeholder="Enter note..."/>
+      <Input
+        placeholder="Enter note title"
+        value={title}
+        onChange={(e)=>{setTitle(e.target.value)}}
+      />
+
+      <TextArea
+        placeholder="Enter note..."
+        value={content}
+        onChange={(e)=>{setContent(e.target.value)}}
+      />
       <div>
-      <Button variant="add">Add</Button>
+      <Button
+        variant="add"
+        onClick={ (e)=>{ e.preventDefault(); notesFunc({...notes,[title]:{content:content}})}  }
+        >Add</Button>
       </div>
     </form>
   )
