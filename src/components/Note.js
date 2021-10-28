2689d5fc66a0acfb72a727f01117f0eef2681075
cezkid <cesar@enrriquez.com>
2021-10-28 11:18:30 -0400

    Add comments. fix spacing

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import Input from "./elements/Input";
 import TextArea from "./elements/TextArea";
 
+/**
+ * Note component holds fields for title and note
+ * @returns 
+ */
 const Note = ({title, content, setTitle, setContent})=> {
-      return (
+  return (
     <>
       <Input
         placeholder="Enter note title"
-
         value={title}
         onChange={(e)=>{setTitle(e.target.value)}}
       />
