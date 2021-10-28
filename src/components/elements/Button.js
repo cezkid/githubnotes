476c00c15389eeac4f962e1751a96249f3415bce
cezkid <cesar@enrriquez.com>
2021-10-28 11:31:26 -0400

    Comments added

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -1,13 +1,17 @@
-//import React, { useState } from "react";
-
+/**
+ * Require the classnames project to make it easier to write conditional classes
+ */
 var classNames = require('classnames');
 
+/**
+ * Button Component returns a single button
+ * The variant prop allows to adjust the color.
+ */
 const Button = ({ children, variant, onClick, ...props })=> {
 
   return(
     <button
       onClick = {onClick}
-    
       className={
         classNames(
         "text-white rounded px-5 py-3 leading-none",
