a8ec277bea0a6af467b0f3debfef34f1f1fc619c
cezkid <cesar@enrriquez.com>
2021-10-28 01:19:13 -0400

    note now returns a component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react";
+import Input from "./elements/Input";
+import TextArea from "./elements/TextArea";
 
-const Note = ()=> {
-  const createNote = ()=> {
+const Note = ({title, content, setTitle, setContent})=> {
 
+  // const [titleField, setTitleField] = useState('');
+  // const [contentField, setContentField] = useState('');
+
+  const createNote = (title,content)=> {
   }
 
   const readNote = ()=> {
@@ -16,7 +21,22 @@ const Note = ()=> {
   const deleteNote = ()=> {
 
   }
-  return { createNote, readNote, updateNote, deleteNote}
+  return (
+    <>
+      <Input
+        placeholder="Enter note title"
+        value={title}
+        onChange={(e)=>{setTitle(e.target.value)}}
+      />
+
+      <TextArea
+        placeholder="Enter note..."
+        value={content}
+        onChange={(e)=>{setContent(e.target.value)}}
+      />
+    </>
+  )
+
 }
 
 export default Note
