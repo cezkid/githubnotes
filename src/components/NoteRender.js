2ccf8ec9d2c255476b8c786f29c1ff266da46d90
cezkid <cesar@enrriquez.com>
2021-10-28 01:21:14 -0400

    include Note component

--- src/components/NoteRender.js
+++ src/components/NoteRender.js
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Button from "./elements/Button";
-import Input from "./elements/Input";
-import TextArea from "./elements/TextArea";
 
-const NoteRender = ()=> {
+import Note from "./Note";
+
+const NoteRender = ({ note, notes })=> {
+
+  const [title,setTitle] = useState(note.title)
+  const [content,setContent] = useState(note.content)
 
   return(
     <div className="flex flex-wrap gap-2">
       <div className="flex flex-col gap-1">
-        <Input/>
-        <TextArea/>
+        <Note title={title} content={content} setTitle={setTitle} setContent={setContent}/>
       </div>
       <div>
-        <Button variant="delete">Delete</Button>
+        <Button
+          variant="delete"
+          onClick={ ()=>{console.log('delete') }}
+          >
+          Delete
+          </Button>
       </div>
     </div>
   )
