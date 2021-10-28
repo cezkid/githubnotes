c587453191a677965f7a9acf2acf05fdf1e26ffd
cezkid <cesar@enrriquez.com>
2021-10-27 21:54:52 -0400

    create note object

---
+++ src/components/Note.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const Note = ()=> {
+  const createNote = ()=> {
+
+  }
+
+  const readNote = ()=> {
+
+  }
+
+  const updateNote = ()=> {
+
+  }
+
+  const deleteNote = ()=> {
+
+  }
+  return { createNote, readNote, updateNote, deleteNote}
+}
+
+export default Note
