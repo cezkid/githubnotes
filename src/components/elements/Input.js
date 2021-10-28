a01ca30685ad614fbe99c5afe76005a0c42069f9
cezkid <cesar@enrriquez.com>
2021-10-27 21:13:49 -0400

    inherit onChange

--- src/components/elements/Input.js
+++ src/components/elements/Input.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
-
-var classNames = require('classnames');
-
-const Input = ({label, placeholder})=> {
+//import React, { useState } from "react";
+const Input = ({label, onChange, placeholder})=> {
 
   return(
     <>
     <label>{label}</label>
     <input
+      onChange={onChange}
       placeholder={placeholder}
       maxLength="255"
       className="p-2 rounded border"/>
