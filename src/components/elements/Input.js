3774e6f40148d068bd8c0f861d59a418c0ba2dae
cezkid <cesar@enrriquez.com>
2021-10-28 11:26:17 -0400

    comments

--- src/components/elements/Input.js
+++ src/components/elements/Input.js
@@ -1,15 +1,18 @@
-//import React, { useState } from "react";
+/**
+ * Custom Input Component returns an input field
+ * max-length is hardcoded for now
+ */
 const Input = ({value, label, onChange, placeholder})=> {
 
   return(
     <>
-    <label>{label}</label>
-    <input
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      maxLength="255"
-      className="p-2 rounded border"/>
+      <label>{label}</label>
+      <input
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        maxLength="255"
+        className="p-2 rounded border"/>
     </>
   )
 }
